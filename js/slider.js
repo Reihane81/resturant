72252695dd30affe5e9e0279b1d337eacbc16d84
taraneh <reihanehamjadi@gmail.com>
2021-05-27 16:00:28 +0430

    commit

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,33 @@
 
-let slider = tns({
-    "my-slider": "#vertical",
+const controls_container = document.querySelector('.bill-controls-container');
+
+const slider = tns({
+    "container": '#bill-container',
     "items": 1,
-    "axis": "vertical",
-    "swipeAngle": false,
+    "nav": false,
+    "axis": 'vertical',
+    'mouseDrag': true,
+    'constrolsPosition': 'bottom',
+    "controlsContainer": controls_container,
     "speed": 400,
-    "button": true
+    "gutter":40,
+    "center": true
 });
+
+//   const slider = tns({
+//     container: "#artist-item-container",
+//     items: 1,
+//     mouseDrag: true,
+//     swipeAngle: false,
+//     fixedWidth: 325,
+//     speed: 300,
+//     nav: true,
+//     navPosition: "bottom",
+//     rewind: true,
+//     controlsPosition: "bootom",
+//     navContainer: navContainer,
+//     controlsContainer: controlsContainer,
+//     // autoplay: true,
+//     // autoplayHoverPause: true,
+//     // autoplayTimeout: 3500,
+// });
